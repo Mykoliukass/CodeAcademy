6312ffceffad4af2ddf13f41ae7df555852ffcca
Mykolas Jerutis <mykolasjerutis@email.com>
2023-10-09 20:48:10 +0300

    Adding first lecutre

---
+++ Lesson_1_09_27/main.py
@@ -0,0 +1,21 @@
+# # print("labas Antanai")
+# # my_name = "Mykolas"
+# # my_age = 36
+# # print(my_name + str(my_age))
+# first_num = 45
+# second_num = 84
+
+# # sum
+# print("Sum " + str(first_num + second_num))
+# # division
+# print("Division " + str(first_num/second_num))
+# # Minus
+# print("Minus " + str(first_num - second_num))
+# #Multiplication
+# print("Multiplication " + str(first_num*second_num))
+# #Power
+# print("Power " + str(first_num**second_num))
+
+my_name = input("Enter my name: ")
+print("my name is: ", my_name)
+
